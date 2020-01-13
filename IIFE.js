cd43b25ab4acb7be0f6b81f65d9f64512e6e26b5
devjosegomez <devjosegomez@gmail.com>
2020-01-13 15:54:58 -0600

    IIFE: Immediately invoked function expression

---
+++ IIFE.js
@@ -0,0 +1,15 @@
+/*
+IIFE:
+    Immediately invoked function expression
+
+    syntax:
+    (funtion x(n1){
+
+        return n1
+    })(5)
+*/
+
+(function add2Numbers(n1, n2){
+    return n1 + n2
+})(13,20)
+
